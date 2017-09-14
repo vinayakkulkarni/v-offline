853753797327c60ac17402c373977bf48cdf1181
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2017-09-14 14:00:34 +0530

    :rocket: le Component

---
+++ src/v-offline.js
@@ -0,0 +1,40 @@
+module.exports = {
+    template: '<div>\
+        <div :class="onlineClass" v-if="state.online"><slot="online"></slot></div>\
+        <div :class="offlineClass" v-if="!state.online"><slot="offline"></slot></div>\
+    </div>',
+    props: {
+        onlineClass: {
+            type: String,
+            default: 'ui success message',
+            required: false
+        },
+        offlineClass: {
+            type: String,
+            default: 'ui error message',
+            required: false
+        }
+    },
+    data() {
+        return {
+            state: {
+                online: navigator.onLine,
+            },
+        };
+    },
+    mounted() {
+        const vm = this;
+        window.addEventListener('load', () => {
+            vm.updateOnlineStatus();
+            window.addEventListener('online', vm.updateOnlineStatus);
+            window.addEventListener('offline', vm.updateOnlineStatus);
+        });
+    },
+    methods: {
+        updateOnlineStatus: function() {
+            const vm = this;
+            vm.state.online = navigator.onLine || false;
+            vm.$emit('detected-condition', vm.state.online);
+        }
+    }
+};

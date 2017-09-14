bb370822d096dd8b8c02895f83f214c65a19d190
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2017-09-14 14:19:26 +0530

    removed shorthand use of v-bind

--- src/v-offline.js
+++ src/v-offline.js
@@ -1,7 +1,7 @@
 module.exports = {
     template: '<div>\
-        <div :class="onlineClass" v-if="state.online"><slot name="online"></slot></div>\
-        <div :class="offlineClass" v-if="!state.online"><slot name="offline"></slot></div>\
+        <div v-bind:class="onlineClass" v-if="state.online"><slot name="online"></slot></div>\
+        <div v-bind:class="offlineClass" v-if="!state.online"><slot name="offline"></slot></div>\
     </div>',
     props: {
         onlineClass: {

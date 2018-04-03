56fa69d131d04dbbee9d825ae9a996a48e75dd6c
Iyong Budiarso <iyongbudiarso@gmail.com>
2018-04-03 18:26:24 +0800

    Triggering change slot

--- src/VOffline.vue
+++ src/VOffline.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="{onlineClass: onLine, offlineClass: !onLine}">
+  <div :class="{onlineClass: isOnline, offlineClass: !isOnline}">
     <slot
-      v-if="onLine"
+      v-if="isOnline"
       name="online"
     />
     <slot
@@ -12,8 +12,7 @@
 </template>
 
 <script>
-const EVENTS = ['online', 'offline', 'load'];
-
+const EVENTS = ['online', 'offline', 'load']
 export default {
   name: 'v-offline',
   props: {
@@ -27,19 +26,19 @@ export default {
     }
   },
   data: () => ({
-    onLine: navigator.onLine || false,
+    isOnline: navigator.onLine || false,
   }),
   mounted() {
-    EVENTS.forEach(event => window.addEventListener(event, this.updateOnlineStatus));
+    EVENTS.forEach(event => window.addEventListener(event, this.updateOnlineStatus))
   },
   beforeDestroy() {
-    EVENTS.forEach(event => window.removeEventListener(event, this.updateOnlineStatus));
+    EVENTS.forEach(event => window.removeEventListener(event, this.updateOnlineStatus))
   },
   methods: {
     updateOnlineStatus() {
-      this.isOnline = navigator.onLine || false;
-      this.$emit('detected-condition', this.isOnline);
+      this.isOnline = navigator.onLine || false
+      this.$emit('detected-condition', this.isOnline)
     }
   }
-};
+}
 </script>

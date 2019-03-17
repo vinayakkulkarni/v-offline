79c4c02fe8979293d5a9b0725f26ad161bac7df0
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2019-03-18 00:15:00 +0530

    Added `commonjs()` plugin while building the package

--- build/rollup.conf.js
+++ build/rollup.conf.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   plugins: [
+    commonjs(),
     vue(),
     babel(),
   ],

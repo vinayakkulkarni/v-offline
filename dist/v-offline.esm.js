ec467f3b7c1422c75eff0306ae78e91751c3c4fc
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-09-19 20:10:52 +0530

    fix(bundle): remove `dist` from git repo

--- dist/v-offline.esm.js
+++ dist/v-offline.esm.js
@@ -1,68 +1,88 @@
-function createCommonjsModule(fn, module) {
-	return module = { exports: {} }, fn(module, module.exports), module.exports;
+function createCommonjsModule(fn, basedir, module) {
+	return module = {
+	  path: basedir,
+	  exports: {},
+	  require: function (path, base) {
+      return commonjsRequire(path, (base === undefined || base === null) ? module.path : base);
+    }
+	}, fn(module, module.exports), module.exports;
+}
+
+function commonjsRequire () {
+	throw new Error('Dynamic requires are not currently supported by @rollup/plugin-commonjs');
 }
 
 var ping = createCommonjsModule(function (module, exports) {
-  var Ping = function Ping(opt) {
+/**
+ * Creates a Ping instance.
+ * @returns {Ping}
+ * @constructor
+ */
+var Ping = function(opt) {
     this.opt = opt || {};
     this.favicon = this.opt.favicon || "/favicon.ico";
     this.timeout = this.opt.timeout || 0;
     this.logError = this.opt.logError || false;
-  };
+};
 
-  Ping.prototype.ping = function (source, callback) {
+/**
+ * Pings source and triggers a callback when completed.
+ * @param source Source of the website or server, including protocol and port.
+ * @param callback Callback function to trigger when completed. Returns error and ping value.
+ * @param timeout Optional number of milliseconds to wait before aborting.
+ */
+Ping.prototype.ping = function(source, callback) {
     var self = this;
     self.wasSuccess = false;
     self.img = new Image();
     self.img.onload = onload;
     self.img.onerror = onerror;
+
     var timer;
     var start = new Date();
 
     function onload(e) {
-      self.wasSuccess = true;
-      pingCheck.call(self, e);
+        self.wasSuccess = true;
+        pingCheck.call(self, e);
     }
 
     function onerror(e) {
-      self.wasSuccess = false;
-      pingCheck.call(self, e);
+        self.wasSuccess = false;
+        pingCheck.call(self, e);
     }
 
     if (self.timeout) {
-      timer = setTimeout(function () {
-        pingCheck.call(self, undefined);
-      }, self.timeout);
-    }
+        timer = setTimeout(function() {
+            pingCheck.call(self, undefined);
+    }, self.timeout); }
 
+
+    /**
+     * Times ping and triggers callback.
+     */
     function pingCheck() {
-      if (timer) {
-        clearTimeout(timer);
-      }
+        if (timer) { clearTimeout(timer); }
+        var pong = new Date() - start;
 
-      var pong = new Date() - start;
-
-      if (typeof callback === "function") {
-        if (!this.wasSuccess) {
-          if (self.logError) {
-            console.error("error loading resource");
-          }
-
-          return callback("error", pong);
+        if (typeof callback === "function") {
+            // When operating in timeout mode, the timeout callback doesn't pass [event] as e.
+            // Notice [this] instead of [self], since .call() was used with context
+            if (!this.wasSuccess) {
+                if (self.logError) { console.error("error loading resource"); }
+                return callback("error", pong);
+            }
+            return callback(null, pong);
         }
-
-        return callback(null, pong);
-      }
     }
 
-    self.img.src = source + self.favicon + "?" + +new Date();
-  };
+    self.img.src = source + self.favicon + "?" + (+new Date()); // Trigger image load with cache buster
+};
 
-  {
+{
     if ( module.exports) {
-      module.exports = Ping;
+        module.exports = Ping;
     }
-  }
+}
 });
 
 var ping_js = ping;
@@ -135,72 +155,79 @@ var script = {
   }
 };
 
-function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-  if (typeof shadowMode !== 'boolean') {
-    createInjectorSSR = createInjector;
-    createInjector = shadowMode;
-    shadowMode = false;
-  }
-
-  var options = typeof script === 'function' ? script.options : script;
-
-  if (template && template.render) {
-    options.render = template.render;
-    options.staticRenderFns = template.staticRenderFns;
-    options._compiled = true;
-
-    if (isFunctionalTemplate) {
-      options.functional = true;
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+    if (typeof shadowMode !== 'boolean') {
+        createInjectorSSR = createInjector;
+        createInjector = shadowMode;
+        shadowMode = false;
     }
-  }
-
-  if (scopeId) {
-    options._scopeId = scopeId;
-  }
-
-  var hook;
-
-  if (moduleIdentifier) {
-    hook = function hook(context) {
-      context = context || this.$vnode && this.$vnode.ssrContext || this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext;
-
-      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-        context = __VUE_SSR_CONTEXT__;
-      }
-
-      if (style) {
-        style.call(this, createInjectorSSR(context));
-      }
-
-      if (context && context._registeredComponents) {
-        context._registeredComponents.add(moduleIdentifier);
-      }
-    };
-
-    options._ssrRegister = hook;
-  } else if (style) {
-    hook = shadowMode ? function (context) {
-      style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
-    } : function (context) {
-      style.call(this, createInjector(context));
-    };
-  }
-
-  if (hook) {
-    if (options.functional) {
-      var originalRender = options.render;
-
-      options.render = function renderWithStyleInjection(h, context) {
-        hook.call(context);
-        return originalRender(h, context);
-      };
-    } else {
-      var existing = options.beforeCreate;
-      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    // Vue.extend constructor export interop.
+    const options = typeof script === 'function' ? script.options : script;
+    // render functions
+    if (template && template.render) {
+        options.render = template.render;
+        options.staticRenderFns = template.staticRenderFns;
+        options._compiled = true;
+        // functional template
+        if (isFunctionalTemplate) {
+            options.functional = true;
+        }
     }
-  }
-
-  return script;
+    // scopedId
+    if (scopeId) {
+        options._scopeId = scopeId;
+    }
+    let hook;
+    if (moduleIdentifier) {
+        // server build
+        hook = function (context) {
+            // 2.3 injection
+            context =
+                context || // cached call
+                    (this.$vnode && this.$vnode.ssrContext) || // stateful
+                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
+            // 2.2 with runInNewContext: true
+            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+                context = __VUE_SSR_CONTEXT__;
+            }
+            // inject component styles
+            if (style) {
+                style.call(this, createInjectorSSR(context));
+            }
+            // register component module identifier for async chunk inference
+            if (context && context._registeredComponents) {
+                context._registeredComponents.add(moduleIdentifier);
+            }
+        };
+        // used by ssr in case component is cached and beforeCreate
+        // never gets called
+        options._ssrRegister = hook;
+    }
+    else if (style) {
+        hook = shadowMode
+            ? function (context) {
+                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
+            }
+            : function (context) {
+                style.call(this, createInjector(context));
+            };
+    }
+    if (hook) {
+        if (options.functional) {
+            // register for functional component in vue file
+            const originalRender = options.render;
+            options.render = function renderWithStyleInjection(h, context) {
+                hook.call(context);
+                return originalRender(h, context);
+            };
+        }
+        else {
+            // inject component registration as beforeCreate hook
+            const existing = options.beforeCreate;
+            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+        }
+    }
+    return script;
 }
 
 /* script */
@@ -232,7 +259,7 @@ __vue_render__._withStripped = true;
   
 
   
-  const __vue_component__ = normalizeComponent(
+  const __vue_component__ = /*#__PURE__*/normalizeComponent(
     { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
     __vue_inject_styles__,
     __vue_script__,

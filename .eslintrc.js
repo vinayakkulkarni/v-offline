c5a77e5430cf59395778d95c4bfc3c7dee14cc86
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-07-22 15:46:26 +0530

    feat: update `eslintrc` 🧽

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,31 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    es6: true,
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    lib: ['es2020'],
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    extraFileExtensions: ['.vue'],
   },
+  plugins: ['vue', 'jsdoc', '@typescript-eslint', 'prettier'],
   extends: [
     'plugin:vue/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:prettier/recommended',
+    'plugin:jsdoc/recommended',
     'prettier',
-    'prettier/vue',
   ],
-  plugins: ['vue', 'prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
 };

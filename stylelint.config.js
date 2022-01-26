e7d1173dda81031c8f83947fa518a2e18f51e9c4
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-01-27 01:47:09 +0530

    feat: update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,34 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+    'stylelint-config-standard-scss',
+    'stylelint-config-html',
+    'stylelint-config-recommended-vue',
+  ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
+    'declaration-block-trailing-semicolon': null,
+    'no-descending-specificity': null,
+    'color-no-invalid-hex': true,
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['font-named-instance'],
+      },
+    ],
+  },
 };

5449e2aaf6c07b5ff00a89213d815261c1fa53b4
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-10-05 00:33:23 +0530

    refactor!: repo housekeeping
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- ship.config.cjs
+++ ship.config.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   installCommand: () => 'npm i --prefer-offline --no-audit --no-optional',
-  publishCommand: () => 'npm publish --tag latest',
+  publishCommand: () => 'npm publish --tag legacy',
 };

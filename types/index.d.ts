e0e20099d6f0c218df6360a5ad72b3799f680736
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-11-06 03:13:48 +0530

    fix: remove types directory ❤️
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import VOffline from './components/VOffline.vue';
-export { default } from './install';
-export { VOffline };
--- types/install.d.ts
+++
@@ -1,3 +0,0 @@
-import { App as Application } from 'vue';
-declare const install: (app: Application) => void;
-export default install;

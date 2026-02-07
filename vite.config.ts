89745f98971b5cf04e0637e9a81b85e90d9bd775
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:17:56 +0530

    refactor: update vite.config.ts for ESM-only output

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,21 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import pkg from './package.json';
-
-const banner = `/*!
-* ${pkg.name} v${pkg.version}
-* (c) ${new Date().getFullYear()} ${pkg.author.name}
-* @license ${pkg.license}
-*/`;
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     target: 'esnext',
-    minify: 'esbuild',
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VOffline',
-      formats: ['es', 'cjs', 'umd', 'iife'],
-      fileName: pkg.name,
-    },
-    commonjsOptions: {
-      extensions: ['.js', '.ts', '.vue'],
-      exclude: 'src/**',
-      include: 'node_modules/**',
+      formats: ['es'],
+      fileName: 'v-offline',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue', 'ping.js'],
       output: {
         exports: 'named',
-        banner,
-        strict: true,
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'vue',
-          'ping.js': 'Ping',
-        },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'style.css') return 'v-offline.css';
           return assetInfo.name;
